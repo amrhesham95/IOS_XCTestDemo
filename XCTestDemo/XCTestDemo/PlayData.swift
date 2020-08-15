9c767e9f64bb5965094c964d01e3b9554933289d
Amr Hesham <amr.hesham@fawry.com>
2020-08-15 17:04:42 +0200

    first test case .. all words are equal to zero

---
+++ XCTestDemo/XCTestDemo/PlayData.swift
@@ -0,0 +1,12 @@
+//
+//  PlayData.swift
+//  XCTestDemo
+//
+//  Created by Amr Hesham on 8/15/20.
+//  Copyright © 2020 Amr Hesham. All rights reserved.
+//
+
+import Foundation
+class PlayData {
+    var allWords = [String]()
+}
--- XCTestDemo/XCTestDemoTests/XCTestDemoTests.swift
+++ XCTestDemo/XCTestDemoTests/XCTestDemoTests.swift
@@ -18,6 +18,11 @@ class XCTestDemoTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testAllWordsLoaded() {
+        let playData = PlayData()
+        XCTAssertEqual(playData.allWords.count, 0, "allWords must be 0")
+    }
 
     func testExample() {
         // This is an example of a functional test case.
